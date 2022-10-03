690480d9b9e2e12736becb55c830379b2d34a746
jesusrpd <jesusrodrigo881@gmail.com>
2022-10-02 21:06:33 -0500

    estructura html del header

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,42 @@
-
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-    </div>
+    <>
+      <header>
+        <nav>
+          <Image src="/icons/logo.svg" alt="" width={100} height={100} />
+          <ul>
+            <li>Home</li>
+            <li>About me</li>
+            <li>Briefcase</li>
+            <li>Trajectory</li>
+            <li>Services</li>
+            <li>Contact me</li>
+          </ul>
+        </nav>
+        <div>
+          <a>
+            <Image src="/icons/whatsapp.svg" alt='icon whatsapp' width={24} height={24}/>
+          </a>
+          <a>
+            <Image src="/icons/instagram.svg" alt='icon instagram' width={24} height={24}/>
+          </a>
+          <a>
+            <Image src="/icons/facebook.svg" alt='icon facebook' width={24} height={24}/>
+          </a>
+          <a>
+            <Image src="/icons/github.svg" alt='icon github' width={24} height={24}/>
+          </a>
+        </div>
+        <div>
+          <p>HELLO I´M</p>
+          <h1>JESÚS RODRIGO</h1>
+          <span>Web developer full stack</span>
+          <button>Download CV</button>
+        </div>
+        <Image src="/img/perfil.png" width={150} height={150} alt="imagen de mi perfil"/>
+      </header>
+    </>
   )
 }
