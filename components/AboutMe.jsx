a33557fc6d2882d8bc71c33170de22e15df0fe71
jesusrpd <jesusrodrigo881@gmail.com>
2022-10-05 22:09:37 -0500

    section aboutme almost finish

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -1,16 +1,16 @@
 export default function AboutMe(){
   return(
-    <main>
-      <div>
-        <h2>ABOUT ME</h2>
-        <p>Soy una persona comprometida, dedicado a lo que me gusta y siempre buscando mejorar.</p>
-        <p>Disfruto trabajar en equipo, esperando aportar desde mi experiencia y tambien aprender de los demas.</p>
+    <main className="w-full h-screen bg-white flex justify-around items-center">
+      <div className="text-center">
+        <h2 className="mb-8">ABOUT ME</h2>
+        <p>Soy una persona comprometida, dedicado<br/> a lo que me gusta y siempre buscando<br/> mejorar.</p>
+        <p>Disfruto trabajar en equipo, esperando<br/> aportar desde mi experiencia y tambien<br/> aprender de los demas.</p>
       </div>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+      <div className="mosaico-container">
+        <div className="mosaico-item"></div>
+        <div className="mosaico-item"></div>
+        <div className="mosaico-item"></div>
+        <div className="mosaico-item"></div>
       </div>
     </main>
   )
