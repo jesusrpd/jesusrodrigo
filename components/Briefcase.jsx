a60efcb0f22969955639d26eabc81d785793aaa5
jesusrpd <jesusrodrigo881@gmail.com>
2022-10-08 23:13:36 -0500

    structure and image witsh colors anda icons of briefcase

--- components/Briefcase.jsx
+++ components/Briefcase.jsx
@@ -1,18 +1,42 @@
+import Image from "next/image";
+
 export default function Briefcase (){
   return(
     <section id="briefcase" className="briefcase w-full h-screen bg-black">
       <h2 className="text-white mb-16">BRIEFCASE</h2>
       <div className="briefcase-container">
-        <div className="briefcase-item-v briefcase-item mr-4">
-
+        <div id="pack" className="briefcase-item-v briefcase-item mr-4">
+          <h2 className="mt-2 ml-2 briefcase-item-title">Pack&Pack</h2>
+          <Image src="/img/pack.png" width={200} height={200} alt="image pack&pack"/>
+          <div className="flex flex-col items-center briefcase-btn">
+            <Image alt="arrow up icon" src="/icons/arrow_up.svg" width={10} height={10}/>
+            <span>Ver más</span>
+          </div>
         </div>
         <div className="briefcase-items">
-          <div className="briefcase-item-h briefcase-item mb-4 mr-4"></div>
-          <div className="briefcase-item-h briefcase-item mr-4"></div>
+          <div id="jared" className="briefcase-item-h briefcase-item mb-4 mr-4">
+            <h2 className="mt-2 ml-2 briefcase-item-title">JARED´S LANDSCAPE</h2>
+            <Image src="/img/jareds.png" width={200} height={100} alt="image jaredslandscape"/>
+            <div className="flex flex-col items-center briefcase-btn">
+              <Image alt="arrow up icon" src="/icons/arrow_up.svg" width={10} height={10}/>
+              <span>Ver más</span>
+            </div>
+          </div>
+          <div id="shop" className="briefcase-item-h briefcase-item mr-4">
+            <h2 className="mt-2 ml-2 briefcase-item-title">Shop Web</h2>
+            <Image src="/img/shop.png" width={200} height={130} alt="image shopweb"/>
+            <div className="flex flex-col items-center briefcase-btn">
+              <Image alt="arrow up icon" src="/icons/arrow_up.svg" width={10} height={10}/>
+              <span>Ver más</span>
+            </div>
+          </div>
         </div>
         <div className="briefcase-items">
-          <div className="briefcase-item-c briefcase-item mb-4"></div>
-          <div className="briefcase-item-c briefcase-item"></div>
+          <div className="briefcase-item-c briefcase-item mb-4">
+          </div>
+          <div id="organize" className="briefcase-item-c briefcase-item">
+            <Image src="/img/organize.png" width={200} height={170} alt="image shopweb"/>
+          </div>
         </div>
       </div>
     </section>
