4c6fc0b980ca03d3ee9fd72d08afbc3b8e064537
jesusrpd <jesusrodrigo881@gmail.com>
2022-10-10 00:24:07 -0500

    icons social networks

--- components/Home.jsx
+++ components/Home.jsx
@@ -1,23 +1,32 @@
 import Image from 'next/image'
 import Nav from './Nav'
+import Link from 'next/link'
 
 export default function Home (){
   return(
     <header id='home' className='bg-black w-full h-screen'>
       <Nav/>
       <div className='icons-container fixed left-5 top-1/4'>
-        <a>
-          <Image src="/icons/whatsapp.svg" alt='icon whatsapp' width={40} height={40}/>
-        </a>
-        <a>
-          <Image src="/icons/instagram.svg" alt='icon instagram' width={40} height={40}/>
-        </a>
-        <a>
-          <Image src="/icons/facebook.svg" alt='icon facebook' width={40} height={40}/>
-        </a>
-        <a>
-          <Image src="/icons/github.svg" alt='icon github' width={40} height={40}/>
-        </a>
+        <Link href="https://api.whatsapp.com/send?phone=5537046181">
+          <a target="_blank">
+            <Image src="/icons/whatsapp.svg" alt='icon whatsapp' width={40} height={40}/>
+          </a>
+        </Link>
+        <Link href="https://www.instagram.com/jesusjrpd">
+          <a target="_blank">
+            <Image src="/icons/instagram.svg" alt='icon instagram' width={40} height={40}/>
+          </a>
+        </Link>
+        <Link href="https://www.facebook.com/profile.php?id=100008217585249">
+          <a target="_blank">
+            <Image src="/icons/facebook.svg" alt='icon facebook' width={40} height={40}/>
+          </a>
+        </Link>
+        <Link href="https://github.com/jesusrpd">
+          <a target="_blank">
+            <Image src="/icons/github.svg" alt='icon github' width={40} height={40}/>
+          </a>
+        </Link>
       </div>
       <div className='flex w-full justify-around h-full items-center'>
         <div className='text-center'>
